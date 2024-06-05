f5ad68ad1be8120154f1d7ef9340117339243292
Piotr Siuszko <siuszko@zoho.com>
2024-06-05 22:58:25 +0200

    fmt

--- src/app.rs
+++ src/app.rs
@@ -296,7 +296,7 @@ impl eframe::App for App {
         egui::SidePanel::left("leftPanel")
             .frame(egui::Frame::canvas(&ctx.style()))
             .show(ctx, |ui| {
-                ui.allocate_space([160.0,TOP_SIDE_MARGIN].into());
+                ui.allocate_space([160.0, TOP_SIDE_MARGIN].into());
                 ui.with_layout(Layout::top_down(eframe::emath::Align::Min), |ui| {
                     for id in ["Favorites", "User", "Drives"] {
                         let Some(collection) = self.locations.get_mut(id) else {
@@ -314,8 +314,11 @@ impl eframe::App for App {
                                         Layout::left_to_right(eframe::emath::Align::Min),
                                         |ui| {
                                             let button = ui.add(
-                                                egui::Button::new(RichText::new(&location.name).strong()).frame(false)
-                                                    .fill(egui::Color32::from_white_alpha(0)),
+                                                egui::Button::new(
+                                                    RichText::new(&location.name).strong(),
+                                                )
+                                                .frame(false)
+                                                .fill(egui::Color32::from_white_alpha(0)),
                                             );
                                             if button.clicked() {
                                                 new_path = Some(location.path.clone());
@@ -338,7 +341,7 @@ impl eframe::App for App {
                                     collection.locations.remove(id);
                                 }
                             });
-                            ui.add_space(15.0);
+                        ui.add_space(15.0);
                     }
                 });
             });
