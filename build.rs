35d7d3d46addd99fb7303c4d4cd5d2e55a95f4d7
MevLyshkin <siuszko@zoho.com>
2024-08-25 21:50:25 +0200

    Expects

--- build.rs
+++ build.rs
@@ -2,8 +2,8 @@ fn main() -> anyhow::Result<()> {
     let output = std::process::Command::new("git")
         .args(["rev-parse", "HEAD"])
         .output()
-        .unwrap();
-    let git_hash = String::from_utf8(output.stdout).unwrap();
+        .expect("Failed to run git command");
+    let git_hash = String::from_utf8(output.stdout).expect("Failed to get output from git command");
     println!("cargo:rustc-env=GIT_HASH={}", &git_hash[..7]);
 
     let target = std::env::var("CARGO_CFG_TARGET_OS")?;
