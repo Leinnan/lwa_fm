9bad9ec9d6f3d4f9e46034f638180775466c1286
Piotr Siuszko <siuszko@zoho.com>
2024-08-25 21:40:42 +0200

    Provide git hash in tooltip

--- build.rs
+++ build.rs
@@ -1,6 +1,13 @@
 extern crate embed_resource;
 
 fn main() -> anyhow::Result<()> {
+    let output = std::process::Command::new("git")
+        .args(["rev-parse", "HEAD"])
+        .output()
+        .unwrap();
+    let git_hash = String::from_utf8(output.stdout).unwrap();
+    println!("cargo:rustc-env=GIT_HASH={}", &git_hash[..7]);
+
     let target = std::env::var("TARGET")?;
     if target.contains("windows") {
         embed_resource::compile("static/icon.rc", embed_resource::NONE);
