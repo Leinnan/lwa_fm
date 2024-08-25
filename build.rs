30ff8dbee4bb62345d0fa0456d591a5a2c9b24c5
Nicolas <bircni@icloud.com>
2024-08-25 21:07:31 +0200

    remove unused build script

--- build.rs
+++
@@ -1,9 +0,0 @@
-extern crate embed_resource;
-
-fn main() -> anyhow::Result<()> {
-    let target = std::env::var("TARGET")?;
-    if target.contains("windows") {
-        embed_resource::compile("static/icon.rc", embed_resource::NONE);
-    }
-    Ok(())
-}
